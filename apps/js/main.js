64bbb60d61c0777286765a1161228d9ae97c0ee5
Yonatan Afewerk Teshome <yonatanafewerk@gmail.com>
2023-05-02 15:46:03 +0300

    font selection and search box done

--- apps/js/main.js
+++ apps/js/main.js
@@ -6,3 +6,30 @@ if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
 } else {
     body.classList.add("light");
 }
+
+// Menu Drop
+const arrow = document.querySelector(".font");
+const menu = document.querySelector(".menu");
+const font = document.querySelector(".font");
+
+let currentFont = "Serif";
+
+window.onload = () => {
+    font.querySelector("p").innerText = currentFont;
+};
+
+arrow.addEventListener("click", (e) => {
+    menu.classList.toggle("hidden");
+    menu.parentNode.querySelector("img").style.transform =
+        menu.classList.contains("hidden") ? "rotate(360deg)" : "rotate(180deg)";
+});
+
+menu.addEventListener("click", (event) => {
+    if (event.target.nodeName === "LI") {
+        const selectedLi = event.target;
+        const selectedText = selectedLi.innerText;
+
+        font.querySelector("p").innerText = selectedText;
+        body.setAttribute("id", selectedText);
+    }
+});
