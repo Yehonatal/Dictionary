c0f2ea04c7708f83b3dec661a57a6057a3fc3036
Yonatan Afewerk Teshome <yonatanafewerk@gmail.com>
2023-05-02 17:44:59 +0300

    theme changer done with a toggle to looks like a sun :)

--- apps/js/main.js
+++ apps/js/main.js
@@ -1,39 +1,55 @@
 const body = document.querySelector("body");
 
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    // body.classList.add("dark");
+/* if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     body.classList.add("light");
 } else {
     body.classList.add("light");
-}
+} */
 
-// Menu Drop
 const arrow = document.querySelector(".font");
 const menu = document.querySelector(".menu");
 const font = document.querySelector(".font");
 
-const storedFontFamily = localStorage.getItem("fontFamily");
-
-window.onload = () => {
-    font.querySelector("p").innerText = storedFontFamily;
-    body.setAttribute("id", storedFontFamily);
-};
-
-arrow.addEventListener("click", (e) => {
+arrow.addEventListener("click", (event) => {
     menu.classList.toggle("hidden");
-    menu.parentNode.querySelector("img").style.transform =
-        menu.classList.contains("hidden") ? "rotate(360deg)" : "rotate(180deg)";
+    const img = menu.parentNode.querySelector("img");
+    img.style.transform = menu.classList.contains("hidden")
+        ? "rotate(360deg)"
+        : "rotate(180deg)";
 });
 
-menu.addEventListener("click", (e) => {
-    if (e.target.nodeName === "LI") {
-        const selectedLi = e.target;
-        const selectedText = selectedLi.innerText;
+menu.addEventListener("click", (event) => {
+    if (event.target.nodeName === "LI") {
+        const selectedText = event.target.innerText;
 
         font.querySelector("p").innerText = selectedText;
         body.setAttribute("id", selectedText);
 
-        // Store the users choice in the local storage
+        // Store the user's choice in local storage
         localStorage.setItem("fontFamily", selectedText);
     }
 });
+
+// Theme picker
+const picker = document.querySelector(".picker");
+const input = picker.querySelector("input");
+
+input.addEventListener("change", (event) => {
+    const themeType = input.checked ? "dark" : "light";
+    document.body.className = themeType;
+    localStorage.setItem("themePreference", themeType);
+});
+
+window.onload = () => {
+    const storedFontFamily = localStorage.getItem("fontFamily");
+    const storedTheme = localStorage.getItem("themePreference");
+    if (storedFontFamily) {
+        font.querySelector("p").innerText = storedFontFamily;
+        body.setAttribute("id", storedFontFamily);
+        body.classList.add(storedTheme);
+    }
+
+    if (body.classList.contains("dark")) {
+        input.value = "checked";
+    }
+};
