33c2867786b6028e4b2633408b63204f1e7a7f16
Yonatan Afewerk Teshome <yonatanafewerk@gmail.com>
2023-05-03 08:39:34 +0300

    data fetch started

--- apps/js/main.js
+++ apps/js/main.js
@@ -51,3 +51,66 @@ window.onload = () => {
         ? (input.checked = true)
         : (input.checked = false);
 };
+
+// Data Access and Render
+const searchBtn = document.querySelector("#search-btn");
+const inputBtn = document.querySelector("#input-btn");
+const main = document.querySelector("#main");
+
+// Get the data from the API
+async function fetchData(word) {
+    const response = await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    );
+    if (response.ok) {
+        const data = await response.json();
+        return data;
+    } else {
+        throw new Error("Network response was not ok.");
+    }
+}
+
+// Render the data to page
+function renderData(data) {
+    console.log(data);
+    // Remove old Renders
+    const containers = document.querySelectorAll(".container");
+    containers.forEach((div) => div.remove());
+
+    // Add New Render
+    data.forEach((entry, index) => {
+        const container = document.createElement("div");
+        container.className = "container";
+
+        const title = document.createElement("h1");
+        title.innerText = titleCase(entry.word);
+
+        const phonetic = document.createElement("h3");
+        phonetic.innerText = entry.phonetic;
+
+        const subtitle = document.createElement("span");
+        subtitle.innerText = `${index + 1}/${data.length}`;
+
+        title.appendChild(subtitle);
+        container.appendChild(title);
+        container.appendChild(phonetic);
+        main.appendChild(container);
+    });
+}
+
+function titleCase(str) {
+    return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}
+
+// Even listener for Search
+searchBtn.addEventListener("click", async (event) => {
+    event.preventDefault();
+    const word = inputBtn.value.trim();
+
+    try {
+        const data = await fetchData(word);
+        renderData(data);
+    } catch (err) {
+        console.log(err);
+    }
+});
